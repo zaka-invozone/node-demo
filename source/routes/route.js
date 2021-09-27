423b74b7fae74b58550ca9a22815d89304091378
Azeem-IZ <m.azeem@invozone.com>
2021-09-27 13:21:34 +0500

    first commit1

---
+++ source/routes/route.js
@@ -0,0 +1,25 @@
+// *******************************************************************
+// "Routes" to forward the supported requests (and any information 
+// encoded in request URLs) to the appropriate controller functions.
+// *******************************************************************
+// import my GET API from the controller functions
+import { home, getTodayDate, getMonthsName } from '../controller/controller.js';
+// set up the routing
+const routes = (app) => {
+    // home page
+    app.route('/')
+        .get(home)
+    // GET home page.
+    app.route('/home')
+        .get(home)
+    // Get today's date
+    app.route('/today')
+        .get(getTodayDate)
+    // get list of month names
+    app.route('/months')
+        .get(getMonthsName)
+    // get list of People
+
+}
+// export the route
+export default routes;
